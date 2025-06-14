142fefa5aafaebd9d8bc7dbaab1310654b3d75c4
Matrix2121 <vasko10.vs38@gmail.com>
2025-06-14 19:18:47 +0300

    Definition of dtos

---
+++ BE/Dtos/CardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SummerPracticeWebApi.Dtos
+{
+    public class CardDto
+    {
+        public uint Id { get; set; }
+        public string CardNumber { get; set; }
+        public string CardHolder { get; set; }
+        public DateOnly Validity { get; set; }
+    }
+}
--- BE/Dtos/PlanningBudgetDto.cs
+++ BE/Dtos/PlanningBudgetDto.cs
@@ -1,17 +1,16 @@
+using SummerPracticeWebApi.Enums;
 using SummerPracticeWebApi.Models;
 
 namespace SummerPracticeWebApi.Models
 {
     public class PlanningBudgetDto
     {
-        public int Amount { get; set; }
+        public uint Amount { get; set; }
         public Currency Currency { get; set; }
         public bool IsIncome { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-
         public string CategoryCode { get; set; }
-
-        public int UserId { get; set; }
+        public uint UserId { get; set; }
     }
 }
