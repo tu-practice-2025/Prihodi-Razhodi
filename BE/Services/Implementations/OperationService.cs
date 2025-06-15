9434dae1694af72af0268801ae92dc6aaac529c6
Matrix2121 <vasko10.vs38@gmail.com>
2025-06-15 14:59:45 +0300

    Operations service implementation

---
+++ BE/Services/Implementations/OperationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SummerPracticeWebApi.DataAccess.Context;
+using SummerPracticeWebApi.Dtos;
+using SummerPracticeWebApi.Mappers;
+using SummerPracticeWebApi.Services.Interfaces;
+
+namespace SummerPracticeWebApi.Services.Implementations
+{
+    public class OperationService : IOperationService
+    {
+        private readonly IncomeExpensesContext _context;
+
+        public OperationService(IncomeExpensesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<OperationDto>> GetOperationsByUserId(uint userId)
+        {
+            return await _context.Operations
+                .Where(operation => operation.Acc.UserId == userId)
+                .Select(operation => OperationMapper.MapToDto(operation))
+                .ToListAsync();
+        }
+    }
+}
---
+++ BE/Services/Interfaces/IOperationService.cs
@@ -0,0 +1,9 @@
+﻿using SummerPracticeWebApi.Dtos;
+
+namespace SummerPracticeWebApi.Services.Interfaces
+{
+    public interface IOperationService
+    {
+        Task<IEnumerable<OperationDto>> GetOperationsByUserId(uint UserId);
+    }
+}
